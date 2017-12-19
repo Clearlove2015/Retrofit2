0251aa43b64a1653d8e0540929a2cd36d2e3b165
Clearlove2015 <1109623941@qq.com>
2017-12-19 21:46:47 +0800

    Retrofit2多图片上传

--- app/src/main/java/com/zc/retrofit2/ApiClient.java
+++ app/src/main/java/com/zc/retrofit2/ApiClient.java
@@ -1,11 +1,14 @@
 package com.zc.retrofit2;
 
+import java.util.Map;
+
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.PartMap;
 import rx.Observable;
 
 /**
@@ -17,5 +20,9 @@ public interface ApiClient {
     @Multipart
     @POST(UriMethod.UPLOAD_IMAGE)
     Observable<ResponseBody> uploadImage(@Part MultipartBody.Part photo, @Part("description") RequestBody description);
+
+    @Multipart
+    @POST("user/photo")
+    Observable<ResponseBody> uploadImages(@PartMap Map<String, RequestBody> photos);
 
 }
--- app/src/main/java/com/zc/retrofit2/MainActivity.java
+++ app/src/main/java/com/zc/retrofit2/MainActivity.java
@@ -1,21 +1,26 @@
 package com.zc.retrofit2;
 
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
 import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.Interceptor;
 import okhttp3.MediaType;
-import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -41,11 +46,23 @@ public class MainActivity extends AppCompatActivity {
         uploadImg();
     }
 
+    public class HeadInterceptor implements Interceptor{
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request request = new Request.Builder()
+                    .addHeader("key","value")
+                    .build();
+            return chain.proceed(request);
+        }
+    }
+
     /**
      * 上传图片
      */
     private void uploadImg() {
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new HeadInterceptor())
                 .addNetworkInterceptor(new StethoInterceptor())
                 .build();
         Retrofit retrofit = new Retrofit.Builder()
@@ -56,11 +73,21 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         ApiClient apiClient = retrofit.create(ApiClient.class);
 
-        File file = new File(Environment.getExternalStorageDirectory(), "wangshu.png");
-        RequestBody photoRequestBody = RequestBody.create(MediaType.parse("image/png"), file);
-        MultipartBody.Part photo = MultipartBody.Part.createFormData("photos", "wangshu.png", photoRequestBody);
+//        File file = new File(Environment.getExternalStorageDirectory(), "wangshu.png");
+//        RequestBody photoRequestBody = RequestBody.create(MediaType.parse("image/png"), file);
+//        MultipartBody.Part photo = MultipartBody.Part.createFormData("photos", "wangshu.png", photoRequestBody);
 
-        apiClient.uploadImage(photo,RequestBody.create(null,"wangshu"))
+        Map<String,RequestBody> imgFils = new HashMap<>();
+        ArrayList<String> paths = new ArrayList<>();
+        paths.add("1.jpg");
+        paths.add("2.jpg");
+        for(String str : paths){
+            File file = new File(str);
+            RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+            imgFils.put("image",requestFile);
+        }
+
+        apiClient.uploadImages(imgFils)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<ResponseBody>() {
